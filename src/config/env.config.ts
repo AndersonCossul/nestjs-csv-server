4f014de45f8bf6bd8eef7a594fad289f20c49a37
Anderson Cossul <anderson_cossul@hotmail.com>
2023-09-06 14:41:47 -0300

    Added env

---
+++ src/config/env.config.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export function validationSchema() {
+  return Joi.object({
+    NODE_ENV: Joi.string()
+      .valid('development', 'production', 'test', 'provision')
+      .default('development'),
+    PORT: Joi.number().default(3000),
+    DATABASE_HOST: Joi.string().required(),
+    DATABASE_PORT: Joi.number().required(),
+    DATABASE_USER: Joi.string().required(),
+    DATABASE_PASSWORD: Joi.string().required(),
+  });
+}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from '../config/env.config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: validationSchema(),
+    }),
+    CatsModule,
+  ],
 })
 export class AppModule {}
